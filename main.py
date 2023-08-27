55128c81e1a18105bc2650b533021e40be72f371
Adam Lindberg <ad.lindberg@gmail.com>
2023-08-27 19:02:40 +0200

    commit

--- main.py
+++ main.py
@@ -7,19 +7,35 @@
 # kontonummer (1234-1234567890) formatvalidering (4 siffror sedan - sedan 10 siffror) saldo
 
 from datetime import datetime
-
+import names
 
 class Customer:
     def __init__(self, name:str,birthdate):
-        self.name = name
-        self.birthdate = birthdate
+        
+        if self.name == None:
+            self.name = self._create_new_name()
+        else:
+            self.name = name
+
+        if self.birthdate == None:
+            self.birthdate = self._create_new_birthdate()
+        else:
+            self.birthdate = birthdate
+
         self.created = datetime.datetime.now()
         self.last_updated = datetime.datetime.now()
-        self.account_no = self._assign_new_account()
-    
+        self.account_no = self._create_new_account()
+        self.balance = 0
 
 
+    def _create_new_name(self):
+        self.name = names.get_first_name()
 
-    def _assign_new_account(self):
+    def _create_new_birthdate(self):
         pass
+    
+    def _create_new_account(self):
+        pass
+
+
         
