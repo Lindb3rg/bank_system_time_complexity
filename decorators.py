a27a0142f86ebc34ed64bc3a1f6eddcfca8b0c10
Adam Lindberg <ad.lindberg@gmail.com>
2023-09-24 17:27:28 +0200

    fixes

--- decorators.py
+++ decorators.py
@@ -14,8 +14,9 @@ def time_logged_function(func:callable):
         seconds,remainder = divmod(elapsed_time,1)
         micro_seconds = remainder * 1e6
         print(f"\nFinished {func.__name__} in {seconds} seconds and {micro_seconds}\n")
+        
         with open("time_log.txt","a") as file:
-            file.write(f"Finished {func.__name__} in {seconds} seconds and micro_seconds\n")
+            file.write(f"Finished {func.__name__} in {seconds} seconds and {micro_seconds}micro seconds\n")
             
 
         return output
